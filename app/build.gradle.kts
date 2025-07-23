b07a732a5ee4280ab3a3a7ddb13e7e077db90dda
joey-wong-cuanschutz <joey.wong@cuanschutz.edu>
2025-07-23 16:10:24 -0600

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.coffeediaries"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.coffeediaries"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //    define the Room lib for db
+    implementation(libs.room.runtime)
+    implementation(libs.androidx.room.compiler)
+
+}
---
+++ app/src/main/java/com/example/coffeediaries/CoffeeRecordsDB.java
@@ -0,0 +1,120 @@
+package com.example.coffeediaries;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class CoffeeRecordsDB {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="id")
+    private int id;
+
+    @ColumnInfo(name = "brewDate")
+    private String brewDate;
+
+    @ColumnInfo(name = "brewMethod")
+    private String brewMethod;
+
+    @ColumnInfo(name = "coffeeName")
+    private String coffeeName;
+
+    @ColumnInfo(name = "brewTime")
+    private String brewTime;
+
+    @ColumnInfo(name = "gramsCoffee")
+    private double gramsCoffee;
+
+    @ColumnInfo(name = "calories")
+    private double calories;
+
+    @ColumnInfo(name = "rating")
+    private double rating;
+
+    @ColumnInfo(name = "comment")
+    private String comment;
+
+    public CoffeeRecordsDB(String brewDate, String brewMethod, String coffeeName, String brewTime, double gramsCoffee, double calories, double rating, String comment) {
+        this.id = 0;
+        this.brewDate = brewDate;
+        this.brewMethod = brewMethod;
+        this.coffeeName = coffeeName;
+        this.brewTime = brewTime;
+        this.gramsCoffee = gramsCoffee;
+        this.calories = calories;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+//    getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBrewDate() {
+        return brewDate;
+    }
+
+    public String getBrewMethod() {
+        return brewMethod;
+    }
+
+    public String getCoffeeName() {
+        return coffeeName;
+    }
+
+    public String getBrewTime() {
+        return brewTime;
+    }
+
+    public double getGramsCoffee() {
+        return gramsCoffee;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+//    setters
+    public void setBrewDate(String brewDate) {
+        this.brewDate = brewDate;
+    }
+
+    public void setBrewMethod(String brewMethod) {
+        this.brewMethod = brewMethod;
+    }
+
+    public void setCoffeeName(String coffeeName) {
+        this.coffeeName = coffeeName;
+    }
+
+    public void setBrewTime(String brewTime) {
+        this.brewTime = brewTime;
+    }
+
+    public void setGramsCoffee(double gramsCoffee) {
+        this.gramsCoffee = gramsCoffee;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+}
